ed06bbdf1a92eaf22791f5d2ac1a43e880b581e0
Grant Petersen-Speelman <grantspeelman@gmail.com>
2023-07-16 12:05:34 +1000

    fix

--- lib/rails_values/railtie.rb
+++ lib/rails_values/railtie.rb
@@ -14,15 +14,15 @@ module RailsValues
 
       ActiveRecord::Type.register(:rv_subdomain) { SimpleStringConverter.new(Subdomain) }
       require_relative 'country_active_model_converter'
-      ActiveRecord::Type.register(:rv_country) { CountryActiveModelConverter }
+      ActiveRecord::Type.register(:rv_country) { CountryActiveModelConverter.new }
       require_relative 'email_address_active_model_converter'
-      ActiveRecord::Type.register(:rv_email_address) { EmailAddressActiveModelConverter }
+      ActiveRecord::Type.register(:rv_email_address) { EmailAddressActiveModelConverter.new }
       require_relative 'public_domain_suffix_active_model_converter'
-      ActiveRecord::Type.register(:rv_public_domain_suffix) { PublicDomainSuffixActiveModelConverter }
+      ActiveRecord::Type.register(:rv_public_domain_suffix) { PublicDomainSuffixActiveModelConverter.new }
       require_relative 'http_url_active_model_converter'
-      ActiveRecord::Type.register(:rv_http_url) { HttpUrlActiveModelConverter }
+      ActiveRecord::Type.register(:rv_http_url) { HttpUrlActiveModelConverter.new }
       require_relative 'currency_casting'
-      ActiveRecord::Type.register(:rv_currency) { CurrencyCasting }
+      ActiveRecord::Type.register(:rv_currency) { CurrencyCasting.new }
     end
   end
 end
