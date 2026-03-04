d9874f8b2274bfac22b7f99b0c60324942ec7c76
bapu-nexl <bapu@nexl.io>
2026-03-04 20:34:18 +1100

    [PLT-341] Reduce memory footprint (#42)
    ## Summary
    Reduce memory footprint for initialize method PublicDomainSuffix
    
    ## Checklist
    - [x] Changelog updated
    - [x] Version bumped in `rails_values.gemspec`
    
    ## Test plan
    No change in behaviour tests already present

--- lib/rails_values/public_domain_suffix.rb
+++ lib/rails_values/public_domain_suffix.rb
@@ -1,5 +1,6 @@
 require 'public_suffix'
 require 'multi_json'
+require 'set'
 
 require_relative 'whole_value_concern'
 require_relative 'exceptional_value'
@@ -15,8 +16,11 @@ module RailsValues
     TOP_LEVEL_DOMAINS =
       (File.readlines("#{__dir__}/tlds-alpha-by-domain.txt").map(&:chomp) + %w[TEST EXAMPLE LOCAL]).freeze
 
+    TOP_LEVEL_DOMAINS_SET = Set.new(TOP_LEVEL_DOMAINS).freeze
+
     def initialize(content)
-      matching_tld = TOP_LEVEL_DOMAINS.any? { |d| content.upcase.split('.').last == d }
+      last_part = content.upcase.split('.').last
+      matching_tld = TOP_LEVEL_DOMAINS_SET.include?(last_part)
       raise ArgumentError, 'has invalid tld' unless matching_tld
 
       @tld_exception = content.include?('.') && PublicSuffix::List.default.find(content).value == content
